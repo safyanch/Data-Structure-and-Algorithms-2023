0d99171116637320f03c996a9e556d71ac610485
Dr. M. Safyan <safyanch@gmail.com>
2023-09-05 12:52:00 +0500

    a

---
+++ a.h
@@ -0,0 +1,13 @@
+using namespace std;
+#include<string>
+class person
+{
+	private:
+		int age;
+		string name;
+	public:
+		int getAge();
+		void setAge(int);
+		string getname();
+		void setname(string );
+};
